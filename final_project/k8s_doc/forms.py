82db632c0fdd384889668c7ec17df8e475a39202
Yun Ho Kang <go5man1058@gmail.com>
2021-05-30 18:34:04 +0900

    test.html, comment.html 수정

--- final_project/k8s_doc/forms.py
+++ final_project/k8s_doc/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 """
 장고는 입력에 대한 처리를 할 수 있도록 폼(form) 기능을 제공
 
-HTML에서 form이란  <form> ... </form> 태그 내에서 우리의 웹사이트를 사용하는 사용자가 
+HTML에서 form이란  <form> ... </form> 태그 내에서 우리의 웹사이트를 사용하는 사용자가
 데이터를 입력할 수 있도록 하고 서버로 데이터를 보내주는 역할을 제공
 """
 
