9eb1e0f33fd3a444b8a6bf2414d466e37c32e98c
Kian <endud5118@gmail.com>
2021-06-02 16:25:13 +0900

    add feedback

--- final_project/front/assets/js/theme.js
+++ final_project/front/assets/js/theme.js
@@ -136,16 +136,7 @@ $('[data-toggle=\'tooltip\']').tooltip({container: 'body'});
 
 /*----
 feedback validation --*/
-$.post($("#contact-us-form").attr('action'), JSON.stringify({
-    name: $("#contact-us-form input[name='name']").val(),
-    email: $("#contact-us-form input[name='email']").val(),
-    phone: $("#contact-us-form input[name='phone']").val(),
-    message: $("#contact-us-form textarea[name='message']").val(),
-    'g-recaptcha-response': $("#contact-us-form textarea[name='g-recaptcha-response']").val()
-}), function (data) {
-    $(".thanks").show();
-    $("#contact-us-form button").hide();
-}, 'json');
+
 /*------------------------
    Scroll to top
 -------------------------- */
