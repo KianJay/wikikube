2301a90b388dbe0eee5aa1539904a3755f63949b
Kian <endud5118@gmail.com>
2021-06-04 12:16:27 +0900

    fix feedback

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -197,3 +197,6 @@ USE_TZ = True
 
 ##CORS ALLOW for test on LOCAL
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = [ 
+'http://localhost:3000', 'https://localhost:3000', 'http://127.0.0.1:8000', ]
