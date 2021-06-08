acaeafc7ea5e23954074a62141b8303103b85f99
Kian <endud5118@gmail.com>
2021-06-08 13:45:34 +0900

    test CI

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -18,7 +18,7 @@ from django.contrib.messages import constants as messages_constants
 import base64
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
